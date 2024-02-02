b47116a21a4251779937447b7149363e6546a1a9
ebetties <ebetties@uncc.edu>
2024-02-02 00:47:32 -0500

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,32 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const myBudget = require('./budget-data.json');
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 375
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
 app.get('/budget', (req,res) => {
-    res.json(budget);
+    res.json(myBudget)
 });
 
 app.listen(port, () => {
